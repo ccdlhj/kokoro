bc875ef6272a94ca76bf6dde76d1459da658b568
ccdlhj <39025641+ccdlhj@users.noreply.github.com>
2025-04-30 12:02:34 +0800

    Update pipeline.py

--- kokoro/pipeline.py
+++ kokoro/pipeline.py
@@ -149,7 +149,7 @@ class KPipeline:
         if voice.endswith('.pt'):
             f = voice
         else:
-            f = hf_hub_download(repo_id=self.repo_id, filename=f'voices/{voice}.pt')
+            f = hf_hub_download(repo_id=self.repo_id, filename=f'voices/{voice}.pt', local_dir="./Kokoro-82M")
             if not voice.startswith(self.lang_code):
                 v = LANG_CODES.get(voice, voice)
                 p = LANG_CODES.get(self.lang_code, self.lang_code)
